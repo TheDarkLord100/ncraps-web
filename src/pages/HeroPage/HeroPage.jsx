a50c37307a03e8c5be19a37e3565224ebcbd63c9
TheDarkLord100 <miracleyt404@gmail.com>
2024-11-25 11:06:39 +0530

    marquee

--- src/pages/HeroPage/HeroPage.jsx
+++ src/pages/HeroPage/HeroPage.jsx
@@ -19,24 +19,27 @@ const HeroSection = () => {
           <>
             <div className="main">
               <div className="main-content">
-              <h3 className="top">6th National Conference</h3>
-              <h3>On</h3>
-              <h1>RECENT ADVANCEMENT IN PHYSICAL SCIENCES</h1>
-              <div className="center-img-container">
-                <img src={NCRAPS} alt="NCRAPS Logo" className="center-img" />
-              </div>
-              <p className="organized-by">
-                <span>jointly organized by</span> <br />
-                School of Physical Sciences, DIT University, Dehradun
-                <br />
-                National Institute of Technology, Uttarakhand
-              </p>
-              <p className="association-with">
-                <span>In association with</span>
-                <br />
-                MRSI, Delhi Chapter
-              </p>
-              <p className="date">14 – 15 February 2025</p>
+                <marquee className="marquee" behavior="scroll" direction="left">
+                  *Participants with valid reasons may be allowed to present their work online at NCRAPS 2025 upon request.*
+                </marquee>
+                <h3 className="top">6th National Conference</h3>
+                <h3>On</h3>
+                <h1>RECENT ADVANCEMENT IN PHYSICAL SCIENCES</h1>
+                <div className="center-img-container">
+                  <img src={NCRAPS} alt="NCRAPS Logo" className="center-img" />
+                </div>
+                <p className="organized-by">
+                  <span>jointly organized by</span> <br />
+                  School of Physical Sciences, DIT University, Dehradun
+                  <br />
+                  National Institute of Technology, Uttarakhand
+                </p>
+                <p className="association-with">
+                  <span>In association with</span>
+                  <br />
+                  MRSI, Delhi Chapter
+                </p>
+                <p className="date">14 – 15 February 2025</p>
               </div>
             </div>
           </>
@@ -67,7 +70,7 @@ const HeroSection = () => {
       <Navbar onTabClick={handleTabClick} />
       <section className="hero">
         <div className="hero-content">
-          {renderContent()} {}
+          {renderContent()} { }
         </div>
       </section>
       {/* <section className='new'>
