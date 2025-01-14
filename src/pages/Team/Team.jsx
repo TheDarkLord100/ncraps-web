ef26e788e9032528ef4fa319d05c402000816cf7
TheDarkLord100 <miracleyt404@gmail.com>
2025-01-14 18:07:24 +0530

    team

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -77,18 +77,6 @@ const teamData = {
         }
     },
     "Website Development": {
-        "Rudraksh Gupta": {
-            "designation": "3rd Year B.Tech CSE",
-            "affiliation": "DIT University"
-        },
-        "Tanya Singhal": {
-            "designation": "4th Year B.Tech CSE",
-            "affiliation": "DIT University"
-        },
-        "Harsheet Sharma": {
-            "designation": "4th Year B.Tech CSE",
-            "affiliation": "DIT University"
-        },
         "Vijayant Singh": {
             "designation": "4th Year B.Tech CSE",
             "affiliation": "DIT University"
